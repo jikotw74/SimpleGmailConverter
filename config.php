e03d151a7eb35d093b1a5bc7e5faab6c4dd248e0
Yi-Te Lu <yite@linkmygoods.com>
2016-12-15 12:17:42 +0800

    add is:unread PARAM_QUERY

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 define('FILES_PATH', 'files/');
 define('SAVED_FILE', 'saved.txt');
-define('PARAM_QUERY', "in:inbox from:(sysop@earth.sinica.edu.tw)");
+define('PARAM_QUERY', "in:inbox is:unread from:(sysop@earth.sinica.edu.tw)");
 ?>
